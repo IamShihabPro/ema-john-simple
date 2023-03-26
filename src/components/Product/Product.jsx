b1bd8ac69301a164996ee685968559cd873bb73b
IamShihabPro <mshihab.pro@gmail.com>
2023-03-27 04:57:42 +0600

    shopping cart complete

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
-  console.log(props.product);
+  //   console.log(props);
   const { img, name, seller, quantity, price, ratings } = props.product;
+  const handleToAddCart = props.handleToAddCart;
+
   return (
     <div className="product">
       <img src={img} alt="" />
@@ -13,7 +15,12 @@ const Product = (props) => {
         <p>Menufacturer: {seller}</p>
         <p>Rating: {ratings} stars</p>
       </div>
-      <button className="btn-cart">Add to Cart</button>
+      <button
+        onClick={() => handleToAddCart(props.product)}
+        className="btn-cart"
+      >
+        Add to Cart{" "}
+      </button>
     </div>
   );
 };
