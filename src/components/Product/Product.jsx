c1028cf7825de0d4b9b87c5429eccc87e13f1f0c
IamShihabPro <mshihab.pro@gmail.com>
2023-03-27 23:28:34 +0600

    project

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Product.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Product = (props) => {
   //   console.log(props);
@@ -19,7 +21,7 @@ const Product = (props) => {
         onClick={() => handleToAddCart(props.product)}
         className="btn-cart"
       >
-        Add to Cart{" "}
+        Add to Cart <FontAwesomeIcon icon={faShoppingCart} />
       </button>
     </div>
   );
